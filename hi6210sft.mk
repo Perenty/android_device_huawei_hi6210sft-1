a0d4030df392823fef9cdb2fe4e199a1295e09c4
hak86 <hakamelassouad@gmail.com>
2016-08-27 16:48:48 +0200

    hi6210sft: add Graphics for boot

--- hi6210sft.mk
+++ hi6210sft.mk
@@ -2,6 +2,47 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
+# AAPT
+PRODUCT_AAPT_CONFIG := xhdpi hdpi normal
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
+# Dalvik
+$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+
+# Display
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+# File System
+PRODUCT_PACKAGES += \
+    	make_ext4fs \
+    	setup_fs
+
+# Graphics
+PRODUCT_COPY_FILES += \
+	device/huawei/hi6210sft/graphics/lib/egl/libGLES_mali.so:system/lib/egl/libGLES_mali.so \
+	device/huawei/hi6210sft/graphics/lib/hw/gralloc.hi6210sft.so:system/lib/hw/gralloc.hi6210sft.so \
+	device/huawei/hi6210sft/graphics/lib/libion.so:system/lib/libion.so \
+	device/huawei/hi6210sft/graphics/lib64/egl/libGLES_mali.so:system/lib64/egl/libGLES_mali.so \
+	device/huawei/hi6210sft/graphics/lib64/hw/gralloc.hi6210sft.so:system/lib64/hw/gralloc.hi6210sft.so \
+	device/huawei/hi6210sft/graphics/lib64/libion.so:system/lib64/libion.so \
+
+PRODUCT_PACKAGES += \
+	libGLES_android \
+	libGLES_mali \
+	libion
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    	debug.hwui.render_dirty_regions=false \
+        hw.lcd.lcd_density=320 \
+    	persist.sys.strictmode.disable=1 \
+    	persist.sys.use_dithering=2 \
+    	ro.opengles.version=131072 \
+        ro.sf.lcd_density=320
+
+# GPS
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
+
 # Kernel
 ifeq ($(TARGET_PREBUILT_KERNEL),)
 LOCAL_KERNEL := device/huawei/hi6210sft/kernel
@@ -20,6 +61,29 @@ PRODUCT_COPY_FILES += \
         device/huawei/hi6210sft/modules/generic_bl.ko:system/modules/generic_bl.ko \
         device/huawei/hi6210sft/modules/lcd.ko:system/modules/lcd.ko \
 
+# Overlay
+DEVICE_PACKAGE_OVERLAYS := $(LOCAL_PATH)/overlay
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    	frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
+    	frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
+    	frameworks/native/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
+    	frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    	frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    	frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    	frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    	frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+    	frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+    	frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    	frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    	frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    	frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    	frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+    	frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    	frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    	frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+
 # Ramdisk
 PRODUCT_COPY_FILES += \
         $(LOCAL_PATH)/ramdisk/fstab.hi6210sft:root/fstab.hi6210sft \
@@ -36,3 +100,17 @@ PRODUCT_COPY_FILES += \
         $(LOCAL_PATH)/ramdisk/sbin/oeminfo_nvm_server:root/sbin/oeminfo_nvm_server \
         $(LOCAL_PATH)/ramdisk/sbin/teecd:root/sbin/teecd \
         $(LOCAL_PATH)/ramdisk/sbin/volisnotd:root/sbin/volisnotd \
+
+# Zygote
+ADDITIONAL_DEFAULT_PROPERTIES += \
+	ro.zygote=zygote64_32
+
+PRODUCT_COPY_FILES += \
+	system/core/rootdir/init.zygote64_32.rc:root/init.zygote64_32.rc
+
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+	ro.zygote=zygote64_32
+
+# USB
+PRODUCT_PACKAGES += \
+ 	com.android.future.usb.accessory
